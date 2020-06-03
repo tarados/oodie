fec3b12c0e22f37b63ccce96e3673014eeecc238
sergey <lith2009@mail.ru>
2020-06-03 21:38:52 +0300

    add frontend code

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <Navbar/>
+  </div>
+</template>
+
+<script>
+    import Navbar from "../components/Navbar";
+
+    export default {
+        name: 'Home',
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style>
+
+
+
+
+
+</style>
