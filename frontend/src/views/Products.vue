400195d40a658b954509d994bcecfe59ba3146d7
sergey <lith2009@mail.ru>
2020-08-25 00:14:30 +0300

    to work 2508

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -6,61 +6,73 @@
     />
     <ProductsList
         thumbnail="small"
-        :category-id="categoryId"
+        :category-id="activeCategoryId"
     />
   </div>
 </template>
 
 <script>
-    import ProductsList from "../components/ProductsList";
-    import Category from "../components/Category";
+import ProductsList from "../components/ProductsList";
+import Category from "../components/Category";
 
-    export default {
-        name: "Products",
-        data() {
-            return {
-                visibleCard: true,
-                category: '',
-                categoryId: null,
-                slug: '',
-                routPath: ''
-            }
-        },
-        components: {
-            ProductsList,
-            Category
-        },
-        methods: {
-            onChoice(index) {
-                if (this.category === this.$store.getters.allCategories[index].title) {
-                    this.category = '';
-                    this.categoryId = null;
-                    this.$router.push({path: '/brands'}).catch(() => {
-                    });
-                } else {
-                    this.category = this.$store.getters.allCategories[index].title;
-                    this.categoryId = this.$store.getters.allCategories[index].id;
-                    this.slug = this.$store.getters.allCategories[index].slug;
-                    this.$router.push({path: '/brands/' + this.slug}).catch(() => {
-                    });
-                }
-            },
-            currentState() {
-                this.slug = this.$route.params.slug;
-            }
-        },
-        watch: {
-            $route(to, from) {
-                console.log(to.params);
-                console.log(from);
-                this.slug = this.$route.params.slug;
-            }
-        },
-        mounted() {
-            this.currentState();
-            console.log(this.slug);
-        }
+export default {
+  name: "Products",
+  data() {
+    return {
+      visibleCard: true,
+      category: '',
+      categoryId: null,
+      slug: ''
     }
+  },
+  components: {
+    ProductsList,
+    Category
+  },
+  computed: {
+    activeCategoryId() {
+      // const activeCategory = this.$store.getters.allCategories.filter(item => item.slug === this.slug)[0];
+      // console.log(activeCategory);
+      let a = 0;
+      const b = this.slug;
+      this.$store.getters.allCategories.forEach(function (item) {
+        if (item.slug === b) {
+          a = item.id
+        }
+      });
+      console.log(a);
+      return a
+    }
+  },
+  methods: {
+    onChoice(index) {
+      if (this.category === this.$store.getters.allCategories[index].title) {
+        this.category = '';
+        this.categoryId = null;
+        this.$router.push({path: '/brands'}).catch(() => {
+        });
+      } else {
+        this.category = this.$store.getters.allCategories[index].title;
+        this.categoryId = this.$store.getters.allCategories[index].id;
+        this.slug = this.$store.getters.allCategories[index].slug;
+        this.$router.push({path: '/brands/' + this.slug}).catch(() => {
+        });
+      }
+    },
+    currentState() {
+      this.activeCategoryId;
+    }
+  },
+  watch: {
+    $route(to, from) {
+      console.log(from.params.slug);
+      this.slug = to.params.slug;
+    }
+  },
+  mounted() {
+    this.currentState();
+  }
+}
 </script>
 
 <style scoped>
