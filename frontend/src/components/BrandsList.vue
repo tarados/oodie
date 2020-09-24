ba56341d443b2dfba9698e949ba852d6a1170695
sergey <lith2009@mail.ru>
2020-09-24 21:53:35 +0300

    breandlist change

--- frontend/src/components/BrandsList.vue
+++ frontend/src/components/BrandsList.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="wrapper">
+  <div class="wrapper-brandlist">
     <div class="row">
       <router-link :to="{name: 'Brand', params: {slug: brand.slug} }"
                    v-for="(brand, index) in brands" :key="index"
                    class="brand"
       >
         <img :src="brand.images">
-        <div class="brand-title">
-          <h4>{{ brand.title }}</h4>
-        </div>
+        <div class="brand-title">{{ brand.title }}</div>
       </router-link>
     </div>
   </div>
@@ -44,150 +42,92 @@ export default {
 </script>
 
 <style scoped>
-.wrapper {
-  margin-top: 3rem;
+.wrapper-brandlist {
+  max-width: 1580px;
+  margin: 0 auto;
 }
 
 h4 {
-  text-transform: uppercase;
-  text-align: center;
+
 }
 
 .row {
-  max-width: 1580px;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  align-items: center;
-  align-content: center;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1em;
+  grid-auto-flow: row;
 }
 
 .brand {
   position: relative;
-  max-height: 31rem;
-  overflow: hidden;
   text-decoration: none;
   color: #3d4246;
-  width: calc((100% / 12) * 4 - 30px);
-  margin: 10px 10px;
 }
 
 .brand-title {
   position: absolute;
   background-color: #c7d9d8;
   width: 72%;
-  height: 4.65rem;
+  height: 12vmin;
   display: flex;
   align-items: center;
   justify-content: center;
   color: white;
-  font-size: 2rem;
+  text-transform: uppercase;
+  text-align: center;
+  font-size: calc(22px + 10 * (100vw / 1580));
+  font-family: 'Arial', sans-serif;
+  bottom: 3vmin;
   left: 14%;
-  top: 83%;
 }
 
-.brand img {
+.brand-image {
+
+}
+
+img {
   width: 100%;
 }
 
 /*media queries******************************************************************************************************/
 @media screen and (max-width: 1024px) {
-  .brand {
-    width: calc((100% / 12) * 6- 30px);
-    max-height: 19rem;
-  }
 
-  .brand-title {
-    top: 82%;
-    height: 2.72rem;
-    font-size: 1rem;
-  }
 }
 
 @media screen and (max-width: 960px) {
-  .brand {
-    width: calc((100% / 12) * 6- 30px);
-  }
 
-  .brand-title {
-    top: 89%;
-    height: 2.72rem;
-    font-size: 1.9rem;
-  }
 }
 
 
-@media screen and (max-width: 770px) {
-  .brand {
-    width: calc((100% / 12) * 6 - 30px);
+@media screen and (max-width: 767px) {
+  .brand-title {
+    font-size: calc(24px + (16 + 16 * 0.7) * (100vw / 1580));
   }
 
-  .brand-title {
-    top: 85%;
-    height: 2.72rem;
-    font-size: 1.8rem;
+  .row {
+    grid-template-columns: repeat(2, 1fr);
   }
 }
 
 @media screen and (max-width: 420px) {
-  .brand {
-    width: auto;
-  }
-
-  .brand-title {
-    top: 86%;
-    height: 2.72rem;
-    font-size: 1.6rem;
+  .row {
+    grid-template-columns: 1fr;
   }
 }
 
 @media screen and (max-width: 375px) {
-  .brand {
-    width: auto;
-  }
 
-  .brand-title {
-    top: 85%;
-    height: 2.72rem;
-    font-size: 1.4rem;
-  }
 }
 
 @media screen and (max-width: 360px) {
-  .brand {
-    width: auto;
-  }
 
-  .brand-title {
-    top: 84%;
-    height: 2.72rem;
-    font-size: 1.3rem;
-  }
 }
 
 @media screen and (max-width: 320px) {
-  .brand {
-   width: auto;
-  }
-
-  .brand-title {
-    top: 82%;
-    height: 2.72rem;
-    font-size: 1.2rem;
-  }
 }
 
 @media screen and (max-width: 280px) {
-  .brand {
-    width: auto;
-  }
 
-  .brand-title {
-    top: 79%;
-    height: 2.72rem;
-    font-size: 1rem;
-  }
 }
 
 
--- frontend/src/views/Brands.vue
+++ frontend/src/views/Brands.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <div class="content-text">
-      <span>ТУТ БУДЕТ ТЕКСТ ПРО ОТБОР БРЕНДОВ ДРУЗЕЙ, КАКИЕ ОРИГИНАЛЬНЫЕ ТУТ ТОВАРЫ И КАК ОНИ ХОРОШИ НА ПОДАРОК
-ТЕКСТ ПРИМЕРНО НА 500 ЗНАКОВ, О ТВОРЧЕСТВЕ НЕБОЛЬШИХ КОМПАНИЙ И УНИКАЛЬНОСТИ, И ПРО ДОСТАВКУ В ОДНОЙ ПОСЫЛКЕ</span>
-    </div>
+  <div class="wrapper-brands">
+    <h3>ТУТ БУДЕТ ТЕКСТ ПРО ОТБОР БРЕНДОВ ДРУЗЕЙ, КАКИЕ ОРИГИНАЛЬНЫЕ ТУТ ТОВАРЫ И КАК ОНИ ХОРОШИ НА ПОДАРОК
+      ТЕКСТ ПРИМЕРНО НА 500 ЗНАКОВ, О ТВОРЧЕСТВЕ НЕБОЛЬШИХ КОМПАНИЙ И УНИКАЛЬНОСТИ, И ПРО ДОСТАВКУ В ОДНОЙ ПОСЫЛКЕ</h3>
     <BrandsList/>
   </div>
 </template>
@@ -48,94 +46,35 @@ export default {
 </script>
 
 <style scoped>
-.content-text {
-  width: 85%;
-  margin: 0 auto;
-  padding: 20px;
-  text-align: justify;
-}
-
-.content-text span {
-  line-height: 1.7;
-}
 
 /*media queries******************************************************************************************************/
 @media screen and (max-width: 1200px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 @media screen and (max-width: 960px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 
 @media screen and (max-width: 750px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 @media screen and (max-width: 420px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 @media screen and (max-width: 375px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 @media screen and (max-width: 360px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 @media screen and (max-width: 320px) {
-  .content-text {
-    width: auto;
-  }
 
-  .content-text span {
-    font-size: 0.7rem;
-    line-height: normal;
-  }
 }
 
 @media screen and (max-width: 280px) {
