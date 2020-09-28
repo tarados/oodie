44fc5e9a7773047a8d0791ac91faa68c8ea82663
Sergey <lith2009@mail.ru>
2020-09-28 16:59:46 +0300

    about us change

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-<div>about us</div>
+  <div class="wrapper">
+    <header class="header">
+      <h1>Есть вопросы?</h1>
+    </header>
+    <main class="content-about">
+      <h2>Мы будем рады получить твое сообщение:</h2>
+      <p>jhkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</p>
+      <h2>Подписывайся на наши страницы:</h2>
+      <p>facebook.com/hoodiyalko/</p>
+      <p>instagram.com/hoodiyalko/</p>
+      <p>tiktok.com/@hoodiyalko/</p>
+      <h2>Возможен самовывоз возле м.Контрактовая площадь по предварительной договоренности.</h2>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -9,5 +22,33 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  font-size: calc(16px + 16 * (100vw / 1600));
+}
 
+h2 {
+  font-size: calc(12px + 12 * (100vw / 1600));
+}
+
+p {
+  font-size: calc(10px + 14 * (100vw / 1600));
+}
+
+.test {
+  font-size: 18px;
+}
+
+@media screen and (max-width: 320px) {
+  h1 {
+    font-size: calc(16px + (16 + 16 * 0.7) * ((100vw - 320px)/ 1600));
+  }
+
+  h2 {
+    font-size: calc(12px + (12 + 12 * 0.7) * ((100vw - 320px) / 1600));
+  }
+
+  p {
+    font-size: calc(10px + (14 + 14 * 0.7) * ((100vw - 320px) / 1600));
+  }
+}
 </style>
