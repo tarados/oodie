c49d14bf0e0587105c1fd5bdcbc2bfc8e8169b94
sergey <lith2009@mail.ru>
2020-10-01 00:02:41 +0300

    productlist change

--- frontend/src/components/ProductsList.vue
+++ frontend/src/components/ProductsList.vue
@@ -57,7 +57,8 @@ export default {
 
 <style scoped>
 .wrapper {
-  margin: 40px auto 60px;
+  max-width: 1240px;
+  margin: 40px auto 40px;
 }
 
 h4 {
@@ -66,47 +67,24 @@ h4 {
 }
 
 .row {
-  margin: 0 0 10% 0;
-  display: grid;
-  grid-template: 600px / repeat(2, 600px);
-  grid-auto-rows: 600px;
-  grid-row-gap: 4em;
-  grid-column-gap: 1.2em;
-  grid-auto-flow: row;
-  justify-content: center;
-}
-
-.row.small {
-  margin: 0 0 2% 0;
-  display: grid;
-  grid-template: 400px / repeat(3, 400px);
-  grid-auto-rows: 400px;
-  grid-row-gap: 4em;
-  grid-column-gap: 1em;
-  grid-auto-flow: row;
-  justify-content: center;
-}
-
-.row img {
-  width: auto;
-  height: 100%;
-}
-
-.image-container {
-  width: 100%;
-  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 .product {
+  width: calc((100% / 12) * 6 - 20px);
+  height: 100%;
+  margin: 0 10px 0 10px;
   text-align: center;
   padding-bottom: 1rem;
   text-decoration: none;
   color: #3d4246;
 }
 
-/*.product img, .item_sm img {*/
-/*  width: 100%;*/
-/*}*/
+.product img {
+  width: 100%;
+  height: 100%;
+}
 
 .price-box {
   width: 100%;
@@ -150,84 +128,40 @@ span.old {
 
 /*media queries*/
 @media screen and (max-width: 1240px) {
-  .row {
-    grid-template: 600px / 600px;
-    grid-auto-rows: 600px;
-  }
-
-  .row.small {
-    grid-template: 400px / repeat(2, 400px);
+  .product {
+    width: calc((100% / 12) * 4 - 20px);
   }
 }
 
 @media screen and (max-width: 860px) {
-  .row.small {
-    grid-template: 400px / 400px;
-  }
 }
 
 @media screen and (max-width: 620px) {
-  .row {
-    grid-template: 450px / 450px;
-    grid-auto-rows: 450px;
-    margin: 0 0 15% 0;
+  .product {
+    width: calc((100% / 12) * 6 - 20px);
   }
 }
 
 @media screen and (max-width: 450px) {
-  .row {
-    grid-template: 380px / 380px;
-    grid-auto-rows: 380px;
-    margin: 0 0 15% 0;
-  }
-
-  .row.small {
-    grid-template: 360px / 360px;
-    grid-auto-rows: 360px;
+  .product {
+    width: calc((100% / 12) * 12 - 20px);
   }
 }
 
 @media screen and (max-width: 375px) {
-  .row {
-    grid-template: 360px / 360px;
-    grid-auto-rows: 360px;
-  }
 
-  .row.small {
-    grid-template: 320px / 320px;
-    grid-auto-rows: 320px;
-  }
 }
 
 @media screen and (max-width: 360px) {
-  .row {
-    grid-template: 320px / 320px;
-    grid-auto-rows: 320px;
-  }
+
 }
 
 @media screen and (max-width: 320px) {
-  .row {
-    grid-template: 280px / 280px;
-    grid-auto-rows: 280px;
-  }
 
-  .row.small {
-    grid-template: 260px / 260px;
-    grid-auto-rows: 260px;
-  }
 }
 
 @media screen and (max-width: 280px) {
-  .row {
-    grid-template: 240px / 240px;
-    grid-auto-rows: 240px;
-  }
 
-  .row.small {
-    grid-template: 220px / 220px;
-    grid-auto-rows: 220px;
-  }
 }
 
 
