e430094b1159d32ec0f8681a544b68070054056b
sergey <lith2009@mail.ru>
2020-10-01 23:23:16 +0300

    footer change

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,53 +1,55 @@
 <template>
   <div class="wrapper-footer">
-    <div class="footer">
-      <div class="contacts">
-        <div class="contacts-item">
-          <span>Контакты:</span>
-        </div>
-        <div class="contacts-item">
-          <img src="../assets/phone-receiver.svg"/>
-          <router-link to="#">
-            <p>+380507204066</p>
-          </router-link>
-        </div>
-        <div class="contacts-item">
-          <img src="../assets/email.svg">
-          <p>hoodiyalko@gmail.com</p>
-        </div>
-      </div>
-      <div class="social">
-        <div class="social-title">
-          <span>Мы в соцсетях:</span>
-        </div>
-        <div class="social-icons">
-          <div class="social-content">
-            <div class="circl">
-              <a href="https://www.facebook.com/hoodiyalko/">
-                <img src="../assets/facebook.png">
-              </a>
-            </div>
+    <div class="container">
+      <div class="footer_row">
+        <div class="contacts">
+          <div class="contacts-item">
+            <span>Контакты:</span>
           </div>
-          <div class="social-content">
-            <div class="circl">
-              <a href="https://www.instagram.com/hoodiyalko/">
-                <img src="../assets/instagram.png">
-              </a>
-            </div>
+          <div class="contacts-item phone">
+            <img src="../assets/phone-receiver.svg"/>
+            <router-link to="#">
+              <p>+380507204066</p>
+            </router-link>
           </div>
-          <div class="social-content">
-            <div class="circl">
-              <a href="https://www.tiktok.com/hoodiyalko/">
-                <img src="../assets/tik-tok.png">
-              </a>
-            </div>
+          <div class="contacts-item">
+            <img src="../assets/email.svg">
+            <p>hoodiyalko@gmail.com</p>
           </div>
         </div>
-      </div>
-      <div class="delivery">
-        <p>Доставка и оплата</p>
-        <p>Сотрудничество</p>
-        <p>Обмен и возврат</p>
+        <div class="delivery">
+          <p>Доставка и оплата</p>
+          <p>Сотрудничество</p>
+          <p>Обмен и возврат</p>
+        </div>
+        <div class="social">
+          <div class="social-title">
+            <span>Мы в соцсетях:</span>
+          </div>
+          <div class="social-icons">
+            <div class="social-content">
+              <div class="circl">
+                <a href="https://www.facebook.com/hoodiyalko/">
+                  <img src="../assets/facebook.png">
+                </a>
+              </div>
+            </div>
+            <div class="social-content">
+              <div class="circl">
+                <a href="https://www.instagram.com/hoodiyalko/">
+                  <img src="../assets/instagram.png">
+                </a>
+              </div>
+            </div>
+            <div class="social-content">
+              <div class="circl">
+                <a href="https://www.tiktok.com/hoodiyalko/">
+                  <img src="../assets/tik-tok.png">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -71,21 +73,33 @@ export default {
 
 <style scoped>
 .wrapper-footer {
-  margin: 0;
   background-color: var(--overlay-color);
 }
 
-.footer {
-  display: grid;
-  grid-template-columns: 1fr 1fr 2fr;
-  grid-template-rows: calc(15vmin + 15 * (100vw / 1838));
-  grid-template-areas: "contacts delivery social";
-  align-items: center;
-  justify-items: center;
+.container {
+  max-width: 1240px;
+  margin: 0 auto;
+}
+
+.footer_row {
+  display: flex;
+}
+
+.contacts,
+.delivery,
+.social {
+  padding: 50px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .contacts {
-  grid-area: contacts;
+  width: 40%;
+}
+
+.delivery {
+  width: 35%;
 }
 
 .contacts-item {
@@ -94,58 +108,49 @@ export default {
   align-items: flex-end;
 }
 
+.phone {
+  padding-bottom: 10px;
+}
+
 .contacts-item a,
 .contacts-item p {
-  margin: 0 1.2vmin;
   text-decoration: none;
   cursor: pointer;
   color: black;
 }
 
+.contacts-item img{
+  margin-right: 15px;
+}
+
 .contacts-item img,
 .circl img {
-  width: calc(2vmin + 8 * (100vw / 1838));
+  width: 22px;
 }
 
 .circl img {
-  margin-top: calc(1vmin + 1 * (100vw / 1838));
+  margin-top: 8px;
 }
 
-.delivery {
-  grid-area: delivery;
-}
-
-.contacts,
-.delivery,
 .social {
-  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-}
-
-.social {
-  grid-area: social;
-  width: calc(15vmin + 15 * (100vw / 1838));
+  justify-content: space-between;
 }
 
 .social-icons {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
 }
 
 .social-content {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: flex-start;
+  margin-right: 15px;
 }
 
 .circl {
   margin: 2% 5% 1% 0;
-  width: calc(3vmin + 14 * (100vw / 1838));
-  height: calc(3vmin + 14 * (100vw / 1838));
+  width: 40px;
+  height: 40px;
   text-align: center;
   background: white;
   border-radius: 50%;
@@ -153,54 +158,42 @@ export default {
 
 span,
 p {
-  font-size: calc(8px + 10 * (100vw / 1838));
+  font-size: calc(10px + 8 * (100vw / 1838));
 }
 
-.contacts span {
+.contacts-item span {
+  padding-bottom: 10px;
   text-transform: uppercase;
 }
 
 /*media queries*****************************************/
 
 @media screen and (max-width: 750px) {
-  .footer {
-    grid-template-rows: calc(15vmin + (15 + 15 * 0.7) * (100vw / 750));
+  .container {
+    width: calc(100vw - 20px);
+    margin: 0 auto;
   }
 
-  span,
-  p {
-    font-size: calc(8px + (10 + 10 * 0.7) * ((100vw - 320px) / 1838));
-  }
-
-  .social {
-    width: calc(15vmin + (15 + 15 * 0.7) * (100vw / 750));
-  }
-
-  .contacts-item img,
-  .circl img {
-    width: 3vmin;
-  }
-
-  .circl img {
-    margin-top: calc(1vmin + 1 * (100vw / 750));
-  }
-
-  .circl {
-    width: calc(3vmin + 14 * (100vw / 750));
-    height: calc(3vmin + 14 * (100vw / 750));
-  }
-}
-
-@media screen and (max-width: 450px) {
-  .footer {
-    grid-template-columns: 1fr 2fr;
-    grid-template-areas: "delivery social";
+  .footer_row {
+    justify-content: space-between;
   }
 
   .contacts {
     display: none;
   }
+
+  span,
+  p {
+    font-size: calc(10px + (8 + 8 * 0.7) * ((100vw - 320px) / 1838));
+  }
+
+  .delivery {
+    width: auto;
+  }
+
+  .social {
+    width: auto;
+  }
+
 }
-
-
 </style>
