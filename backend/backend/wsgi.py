bf05a062a43ffacc6ff0f0239aca2ceb1372d27f
Dmitry Zvada <dmitryzvada@gmail.com>
2020-11-07 14:52:17 +0200

    debugging dotenv

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -14,6 +14,9 @@ from django.core.wsgi import get_wsgi_application
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
+print("!!!dotenv path")
+print(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
