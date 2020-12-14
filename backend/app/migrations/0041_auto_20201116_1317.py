56c94a6591454a064dac2739165c9e820c68d7fd
sergey <lith2009@mail.ru>
2020-12-14 19:42:05 +0300

    fix migration 41

--- backend/app/migrations/0041_auto_20201116_1317.py
+++ backend/app/migrations/0041_auto_20201116_1317.py
@@ -11,9 +11,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.DeleteModel(
-            name='Sale',
-        ),
-        migrations.DeleteModel(
             name='SaleSummary',
         ),
     ]
