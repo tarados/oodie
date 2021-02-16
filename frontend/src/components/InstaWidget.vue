b35c74c613aa4024768d523fb9cbbc6ae2667814
POMDIR <lith2009@mail.ru>
2021-02-16 15:46:22 +0200

    to home 1602

--- frontend/src/components/InstaWidget.vue
+++ frontend/src/components/InstaWidget.vue
@@ -5,16 +5,24 @@
            v-for="(slider, index) in allProducts"
            :key="index"
       >
-        <img :src="slider.image" width="340px">
+        <img :src="slider.image" width="335px">
       </div>
     </div>
-    <svg viewBox="4 0 8 16" width="12" height="16" class="eapps-instagram-feed-posts-slider-nav-icon">
-      <path d="M11.7,7.3l-6-5.9c-0.4-0.4-1.1-0.4-1.5,0c-0.4,0.4-0.4,1.1,0,1.5L9.5,8l-5.2,5.2
+    <div class="prev" v-if="currentSlideIndex >= 1"
+         @click="prevSlide">
+      <svg viewBox="4 0 8 16" width="12" height="16" class="eapps-instagram-feed-posts-slider-nav-icon">
+        <path d="M4.3,8.7l6,5.9c0.4,0.4,1.1,0.4,1.5,0c0.4-0.4,0.4-1.1,0-1.5L6.5,8l5.2-5.2c0.4-0.4,0.4-1.1,0-1.5
+        c-0.4-0.4-1.1-0.4-1.5,0l-6,6C3.9,7.7,3.9,8.3,4.3,8.7z"></path>
+      </svg>
+      <div class="radius-prev"></div>
+    </div>
+    <div class="next" @click="nextSlide">
+      <svg viewBox="4 0 8 16" width="12" height="16" class="eapps-instagram-feed-posts-slider-nav-icon">
+        <path d="M11.7,7.3l-6-5.9c-0.4-0.4-1.1-0.4-1.5,0c-0.4,0.4-0.4,1.1,0,1.5L9.5,8l-5.2,5.2
         c-0.4,0.4-0.4,1.1,0,1.5c0.4,0.4,1.1,0.4,1.5,0l6-6C12.1,8.3,12.1,7.7,11.7,7.3z"></path>
-    </svg>
-    <div class="radius"></div>
-    <button @click="prevSlide">Prev</button>
-    <button @click="nextSlide">Next</button>
+      </svg>
+      <div class="radius-next"></div>
+    </div>
   </div>
 </template>
 
@@ -39,7 +47,8 @@ export default {
       }
     },
     nextSlide() {
-      if (this.currentSlideIndex >= (this.allProducts.length - 1) / 5) {
+      let slideCount = Math.ceil(this.allProducts.length / 5);
+      if (this.currentSlideIndex >= slideCount) {
         this.currentSlideIndex = 0;
       } else {
         this.currentSlideIndex++;
@@ -54,7 +63,6 @@ export default {
 <style>
 .slider-wrapper {
   margin: 0 auto;
-  width: 95%;
   overflow: hidden;
   position: relative;
 }
@@ -67,12 +75,12 @@ export default {
 }
 
 .slider .slider_item {
-  width: 340px;
-  height: 340px;
+  width: 335px;
+  height: 335px;
 }
 
-.slider .slider_item:nth-child(odd) {
-  margin: 0 9px 0;
+.slider .slider_item img:nth-child(2n) {
+  border: 15px solid white;
 }
 
 .slider .slider_item img {
@@ -82,22 +90,42 @@ export default {
   object-fit: cover;
 }
 
-.slider-wrapper svg {
+.slider-wrapper .next svg {
+  z-index: 1;
   position: absolute;
-  top: 45%;
-  right: 0;
-  fill: rgb(255, 255, 255)
+  top: 167px;
+  right: 5px;
+  fill: rgb(255, 255, 255);
 }
 
-.slider-wrapper .radius {
+.slider-wrapper .radius-next {
   position: absolute;
-  top: 45%;
+  top: 145px;
   right: 0;
-  width: 5em;  /* ширина в два раза меньше высоты, иначе получится полуовал */
-  height: 10em;
-  border: 2px solid red;
+  width: 30px;  /* ширина в два раза меньше высоты, иначе получится полуовал */
+  height: 60px;
+  border-radius: 100% 0 0 100% / 50% 0 0 50%;
+  background: #000000;
+  cursor: pointer;
+}
+
+.slider-wrapper .prev svg {
+  z-index: 1;
+  position: absolute;
+  top: 167px;
+  left: 5px;
+  fill: rgb(255, 255, 255);
+}
+
+.slider-wrapper .radius-prev {
+  position: absolute;
+  top: 145px;
+  left: 0;
+  width: 30px;  /* ширина в два раза меньше высоты, иначе получится полуовал */
+  height: 60px;
   border-radius: 0 100% 100% 0 / 0 50% 50% 0;
-  background: mistyrose;
+  background: #000000;
+  cursor: pointer;
 }
 
 </style>
