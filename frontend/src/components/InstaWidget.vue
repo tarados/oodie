63986e4efc09ea97e7818cc165acfccd64420273
sergey <lith2009@mail.ru>
2021-02-18 21:09:10 +0300

    to work 1802

--- frontend/src/components/InstaWidget.vue
+++ frontend/src/components/InstaWidget.vue
@@ -6,6 +6,7 @@
            :key="index"
            :style="`background-image: url(${slider.image})`"
       >
+        <div class="slider_item__background"></div>
       </div>
     </div>
     <div class="prev" v-if="currentSlideIndex >= 1"
@@ -62,7 +63,7 @@ export default {
 
 <style>
 .slider-wrapper {
-  margin: 0 auto;
+  margin: 30px auto;
   overflow: hidden;
   position: relative;
 }
@@ -74,6 +75,7 @@ export default {
 }
 
 .slider .slider_item {
+  position: relative;
   flex: 1 0 auto;
   width: 20%;
   height: 331px;
@@ -81,8 +83,32 @@ export default {
   background-size: cover;
 }
 
+.slider .slider_item .slider_item__background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  background-color: #000000;
+}
+
+.slider .slider_item .slider_item__background:hover {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.8;
+  background-color: #000000;
+}
+
 .slider .slider_item:nth-child(2n) {
   border: 15px solid white;
+}
+
+.slider .slider_item:nth-child(2n):hover {
+  border: 0;
 }
 
 .slider-wrapper .next svg {
