00ad7beeb1aae0299ae03539abbf6a76dc07551b
POMDIR <lith2009@mail.ru>
2021-05-06 16:10:58 +0300

    module insta-widget-fetch change

--- insta-widget-fetch/insta.js
+++ insta-widget-fetch/insta.js
@@ -1,20 +1,19 @@
+require('dotenv').config();
 const Instagram = require('instagram-web-api');
 const fs = require('fs');
 const request = require('request');
 const download = (url, path, callback) => {
     request.head(url, (err, res, body) => {
         request(url)
-          .pipe(fs.createWriteStream(path))
-          .on('close', callback)
+            .pipe(fs.createWriteStream(path))
+            .on('close', callback)
     })
 };
-
 const client = new Instagram({username: 'zvada53', password: 'root2827'});
 (async () => {
     await client.login();
     const photos = await client.getPhotosByUsername({username: 'hoodiyalko'});
     const publicList = photos.user.edge_owner_to_timeline_media.edges;
-    console.log(publicList);
     let photosList = [];
     publicList.forEach((item, index) => {
         const url = item.node.thumbnail_src;
@@ -23,7 +22,7 @@ const client = new Instagram({username: 'zvada53', password: 'root2827'});
             console.log('✅ Done!')
         });
         photosList.push({
-            image: 'http://localhost:8000' + process.env.PATH_MEDIA + path,
+            image: process.env.SITE_URL + process.env.PATH_MEDIA + path,
             comment: item.node.edge_media_to_caption.edges[0].node.text,
             shortcode: item.node.shortcode
         });
