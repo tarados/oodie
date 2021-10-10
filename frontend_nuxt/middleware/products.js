ee655825732e0fda9c7c2df71efa5900022d74f8
sergey <lith2009@mail.ru>
2021-10-10 13:43:15 +0300

    fixed refresh productsList

--- frontend_nuxt/middleware/products.js
+++ frontend_nuxt/middleware/products.js
@@ -1,6 +1,4 @@
 export default async function ({context, store, i18n, route}) {
-  if (process.server) {
-    await store.dispatch('products/fetch');
-    await store.dispatch('categories/fetch');
-  }
+  await store.dispatch('products/fetch');
+  await store.dispatch('categories/fetch');
 }
