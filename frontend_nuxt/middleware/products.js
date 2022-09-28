33942a07b613281cb705c144d4058db59130e69c
POMDIR <lith2009@mail.ru>
2022-09-28 14:50:30 +0300

    USE_TZ = False

--- frontend_nuxt/middleware/products.js
+++ frontend_nuxt/middleware/products.js
@@ -1,6 +1,11 @@
 export default async function ({context, store, i18n, route}) {
+  console.log('begin');
   await store.dispatch('products/fetch');
+  console.log('products ok');
   await store.dispatch('categories/fetch');
+  console.log('categories ok')
   await store.dispatch('home/fetch');
+  console.log('home ok')
   await store.dispatch('description/fetch');
+  console.log('description ok')
 }
